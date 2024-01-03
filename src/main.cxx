6ec01cca2ed05f2fc9b0c681d2580316dfa450ab
ResCXsention <gnsillador@proton.me>
2024-01-02 22:42:02 -0800

    admin: add glad and glfw
    GLAD and GLFW are two crucial libraries for accessing OpenGL functions and creating a window respectively.

--- src/main.cxx
+++ src/main.cxx
@@ -1,8 +1,24 @@
+#include <cstddef>
 #include <iostream>
-import midnight;
+
+#define GLFW_INCLUDE_NONE
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-	
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	GLFWwindow *mw{glfwCreateWindow(800, 600, "The Amaranthine Project", NULL, NULL)};
+	glfwMakeContextCurrent(mw);
+	const int gll{gladLoadGL(glfwGetProcAddress)};
+
+	while (!glfwWindowShouldClose(mw)) {
+
+	}
+
+	glfwDestroyWindow(mw);
+	glfwTerminate();
 	return 0;
 }
