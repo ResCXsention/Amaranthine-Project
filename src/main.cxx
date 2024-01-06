6173d5171f713a46d2dfc5847bb33c1b7873c70e
ResCXsention <gnsillador@proton.me>
2024-01-05 21:30:10 -0800

    admin: change to glbinding

--- src/main.cxx
+++ src/main.cxx
@@ -2,20 +2,36 @@
 #include <iostream>
 
 #define GLFW_INCLUDE_NONE
-#include <glad/gl.h>
+#include <glbinding/gl/gl.h>
+#include <glbinding/glbinding.h>
 #include <GLFW/glfw3.h>
+
+namespace glfwcbs
+{
+	void framebufferSizeCallback(GLFWwindow *win, int w, int h)
+	{
+		gl::glViewport(0, 0, default_win_w, default_win_h);
+	}
+}
 
 int main()
 {
+	const int default_win_w{800};
+	const int default_win_h{600};
+
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-	GLFWwindow *mw{glfwCreateWindow(800, 600, "The Amaranthine Project", NULL, NULL)};
+	GLFWwindow *mw{glfwCreateWindow(default_win_w, default_win_h, "The Amaranthine Project", NULL, NULL)};
+	glfwSetFramebufferSizeCallback(mw, glfwcbs::framebufferSizeCallback);
 	glfwMakeContextCurrent(mw);
-	const int gll{gladLoadGL(glfwGetProcAddress)};
+	glbinding::initialize(glfwGetProcAddress);
+	gl::glViewport(0, 0, default_win_w, default_win_h);
 
 	while (!glfwWindowShouldClose(mw)) {
 
+		glfwSwapBuffers(mw);
+		glfwPollEvents();
 	}
 
 	glfwDestroyWindow(mw);
