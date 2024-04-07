4789defb62d6d4db6497c3689b0fbf15f2fad89e
ResCXsention <gnsillador@proton.me>
2024-04-06 19:01:57 -0700

    feat: add mesh class
    Implemented a simple mesh object that stores untextured mesh data as a group of vertices, indices, and normals.
    
    The sample render of the lit sphere in main.cxx is updated to make use of this. The sphere generation algorithm
    has also been moved as a debug function that is included along with the mesh class.

---
+++ src/mesh.hxx
@@ -0,0 +1,36 @@
+#ifndef RES_MESH
+#define RES_MESH
+
+#include <vector>
+
+namespace res
+{
+	class Mesh final
+	{
+	public:
+		explicit Mesh(
+				const std::vector<float> vertices,
+				const std::vector<float> normals,
+				const std::vector<unsigned int> indices
+				);
+
+		unsigned int get_vao() const;
+
+	private:
+		std::vector<float> vertices;
+		std::vector<float> normals;
+		std::vector<unsigned int> indices;
+
+		unsigned int vao, vbo, ebo, nbo;
+	};
+
+#ifndef NDEBUG
+	Mesh spherical_mesh(
+		const float radius,
+		const unsigned int longitudes,
+		const unsigned int latitudes
+		);
+#endif
+}
+
+#endif
