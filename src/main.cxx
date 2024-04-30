530fbf1b85449dced2208f2dc4900ebd5fc30a5d
ResCXsention <gnsillador@proton.me>
2024-04-29 20:23:43 -0700

    fix: destroy resource controllers before glfwTerminate()
    Enclosing all program code within its own scope allows resource controllers to automatically release themselves
    before glfw is deinitialised, avoiding a (albeit seemingly harmless) end-of-program segmentation fault.

--- src/main.cxx
+++ src/main.cxx
@@ -52,89 +52,91 @@ int main()
 	gl::glCullFace(gl::GL_BACK);
 	gl::glEnable(gl::GL_DEPTH_TEST);
 
-	res::ResourceController<res::TextResource> rc;
-	rc.index("s_vertex", "../src/shaders/vertex.glsl");
-	rc.index("s_fragment", "../src/shaders/fragment.glsl");
-	rc.index("s_water_fragment", "../src/shaders/water_fragment.glsl");
-	const char *vertex_source{rc.retrieve("s_vertex").lock()->get_text()};
-	const char *fragment_source{rc.retrieve("s_fragment").lock()->get_text()};
-	unsigned int program{gl::glCreateProgram()};
-	unsigned int vertex_shader{gl::glCreateShader(gl::GL_VERTEX_SHADER)};
-	unsigned int fragment_shader{gl::glCreateShader(gl::GL_FRAGMENT_SHADER)};
-	gl::glShaderSource(vertex_shader, 1, &vertex_source, NULL);
-	gl::glCompileShader(vertex_shader);
-	gl::glShaderSource(fragment_shader, 1, &fragment_source, NULL);
-	gl::glCompileShader(fragment_shader);
-	gl::glAttachShader(program, vertex_shader);
-	gl::glAttachShader(program, fragment_shader);
-	gl::glLinkProgram(program);
-	unsigned int water_program{gl::glCreateProgram()};
-	const char *water_fragment_source{rc.retrieve("s_water_fragment").lock()->get_text()};
-	unsigned int water_fragment_shader{gl::glCreateShader(gl::GL_FRAGMENT_SHADER)};
-	gl::glShaderSource(water_fragment_shader, 1, &water_fragment_source, NULL);
-	gl::glCompileShader(water_fragment_shader);
-	gl::glAttachShader(water_program, vertex_shader);
-	gl::glAttachShader(water_program, water_fragment_shader);
-	gl::glLinkProgram(water_program);
-	gl::glDeleteShader(vertex_shader);
-	gl::glDeleteShader(fragment_shader);
-	gl::glDeleteShader(water_fragment_shader);
+	{
+		res::ResourceController<res::TextResource> rc;
+		rc.index("s_vertex", "../src/shaders/vertex.glsl");
+		rc.index("s_fragment", "../src/shaders/fragment.glsl");
+		rc.index("s_water_fragment", "../src/shaders/water_fragment.glsl");
+		const char *vertex_source{rc.retrieve("s_vertex").lock()->get_text()};
+		const char *fragment_source{rc.retrieve("s_fragment").lock()->get_text()};
+		unsigned int program{gl::glCreateProgram()};
+		unsigned int vertex_shader{gl::glCreateShader(gl::GL_VERTEX_SHADER)};
+		unsigned int fragment_shader{gl::glCreateShader(gl::GL_FRAGMENT_SHADER)};
+		gl::glShaderSource(vertex_shader, 1, &vertex_source, NULL);
+		gl::glCompileShader(vertex_shader);
+		gl::glShaderSource(fragment_shader, 1, &fragment_source, NULL);
+		gl::glCompileShader(fragment_shader);
+		gl::glAttachShader(program, vertex_shader);
+		gl::glAttachShader(program, fragment_shader);
+		gl::glLinkProgram(program);
+		unsigned int water_program{gl::glCreateProgram()};
+		const char *water_fragment_source{rc.retrieve("s_water_fragment").lock()->get_text()};
+		unsigned int water_fragment_shader{gl::glCreateShader(gl::GL_FRAGMENT_SHADER)};
+		gl::glShaderSource(water_fragment_shader, 1, &water_fragment_source, NULL);
+		gl::glCompileShader(water_fragment_shader);
+		gl::glAttachShader(water_program, vertex_shader);
+		gl::glAttachShader(water_program, water_fragment_shader);
+		gl::glLinkProgram(water_program);
+		gl::glDeleteShader(vertex_shader);
+		gl::glDeleteShader(fragment_shader);
+		gl::glDeleteShader(water_fragment_shader);
 
-	gl::glUseProgram(program);
-	midnight::Matrix4x4 m{midnight::matrixTranslation(midnight::Vector3{0, 0, -10})};
-	m *= midnight::matrixRotation(midnight::Vector3{-1, 0, 0}, 0.1);
-	midnight::Matrix4x4 v{midnight::matrixIdentity<4>()};
-	midnight::Matrix4x4 p{midnight::matrixPerspective(0.57, default_win_w / default_win_h, 0.001F, 2000)};
-	// gl::glUniformMatrix4fv(gl::glGetUniformLocation(program, "u_model"), 1, false, m.dataPtr());
-	// gl::glUniformMatrix4fv(gl::glGetUniformLocation(program, "u_view"), 1, false, v.dataPtr());
-	// gl::glUniformMatrix4fv(gl::glGetUniformLocation(program, "u_projection"), 1, false, p.dataPtr());
+		gl::glUseProgram(program);
+		midnight::Matrix4x4 m{midnight::matrixTranslation(midnight::Vector3{0, 0, -10})};
+		m *= midnight::matrixRotation(midnight::Vector3{-1, 0, 0}, 0.1);
+		midnight::Matrix4x4 v{midnight::matrixIdentity<4>()};
+		midnight::Matrix4x4 p{midnight::matrixPerspective(0.57, default_win_w / default_win_h, 0.001F, 2000)};
+		// gl::glUniformMatrix4fv(gl::glGetUniformLocation(program, "u_model"), 1, false, m.dataPtr());
+		// gl::glUniformMatrix4fv(gl::glGetUniformLocation(program, "u_view"), 1, false, v.dataPtr());
+		// gl::glUniformMatrix4fv(gl::glGetUniformLocation(program, "u_projection"), 1, false, p.dataPtr());
 
-	res::Scene main_scene;
+		res::Scene main_scene;
 
-	res::Node *hull{main_scene.get_root()->add_child()}, *rear_turret{hull->add_child()}, *forward_turret{hull->add_child()};
-	hull->add_component<res::Drawable>();
-	rear_turret->add_component<res::Drawable>();
-	forward_turret->add_component<res::Drawable>();
-	hull->get_component<res::Drawable>()->set_shader(program);
-	rear_turret->get_component<res::Drawable>()->set_shader(program);
-	forward_turret->get_component<res::Drawable>()->set_shader(program);
+		res::Node *hull{main_scene.get_root()->add_child()}, *rear_turret{hull->add_child()}, *forward_turret{hull->add_child()};
+		hull->add_component<res::Drawable>();
+		rear_turret->add_component<res::Drawable>();
+		forward_turret->add_component<res::Drawable>();
+		hull->get_component<res::Drawable>()->set_shader(program);
+		rear_turret->get_component<res::Drawable>()->set_shader(program);
+		forward_turret->get_component<res::Drawable>()->set_shader(program);
 
-	res::Node *camera{rear_turret->add_child()};
-	camera->add_component<res::Camera>();
-	camera->get_component<res::Camera>()->set_active();
-	camera->set_main_transform(midnight::matrixTranslation(midnight::Vector3{0, 0.2, 0}));
-	camera->transform_priority(midnight::matrixRotation(midnight::Vector3{0, 0, 1}, -0.3));
-	camera->transform_priority(midnight::matrixRotation(midnight::Vector3{0, 1, 0}, std::numbers::pi_v<float> / -2));
-	camera->get_component<res::Camera>()->set_fov(2);
-	
-	res::ResourceController<res::ModelResource> mc;
-	mc.index("m_boat", "boat.obj");
-	mc.index("m_ags", "ags.obj");
-	hull->get_component<res::Drawable>()->set_model(mc.retrieve("m_boat"));
-	rear_turret->get_component<res::Drawable>()->set_model(mc.retrieve("m_ags"));
-	forward_turret->get_component<res::Drawable>()->set_model(mc.retrieve("m_ags"));
-	
-	hull->set_main_transform(m);
-	rear_turret->set_priority_transform(midnight::matrixRotation(midnight::Vector3{0, 1, 0}, std::numbers::pi_v<float> / -4));
-	forward_turret->set_priority_transform(midnight::matrixRotation(midnight::Vector3{0, 1, 0}, std::numbers::pi_v<float> / 3));
-	rear_turret->set_main_transform(midnight::matrixTranslation(midnight::Vector3{-0.61, 0.15, 0}));
-	forward_turret->set_main_transform(midnight::matrixTranslation(midnight::Vector3{-1.15, 0.15, 0}));
-
-	while (!glfwWindowShouldClose(mw)) {
-		gl::glClear(gl::GL_COLOR_BUFFER_BIT | gl::GL_DEPTH_BUFFER_BIT);
-
-		const float t{static_cast<const float>(glfwGetTime())};
-		midnight::Vector3 dir{midnight::cartesian3({1, std::sin(t), std::sin(t)})};
-		gl::glUniform3fv(gl::glGetUniformLocation(program, "u_light_dir"), 1, dir.dataPtr());
-		// m *= midnight::matrixRotation(midnight::Vector3{0, 1, 0}, 0.1);
+		res::Node *camera{rear_turret->add_child()};
+		camera->add_component<res::Camera>();
+		camera->get_component<res::Camera>()->set_active();
+		camera->set_main_transform(midnight::matrixTranslation(midnight::Vector3{0, 0.2, 0}));
+		camera->transform_priority(midnight::matrixRotation(midnight::Vector3{0, 0, 1}, -0.3));
+		camera->transform_priority(midnight::matrixRotation(midnight::Vector3{0, 1, 0}, std::numbers::pi_v<float> / -2));
+		camera->get_component<res::Camera>()->set_fov(2);
 		
-		main_scene.cycle();
-		rear_turret->transform_priority(midnight::matrixRotation(midnight::Vector3{0, 1, 0}, 0.1));
+		res::ResourceController<res::ModelResource> mc;
+		mc.index("m_boat", "boat.obj");
+		mc.index("m_ags", "ags.obj");
+		hull->get_component<res::Drawable>()->set_model(mc.retrieve("m_boat"));
+		rear_turret->get_component<res::Drawable>()->set_model(mc.retrieve("m_ags"));
+		forward_turret->get_component<res::Drawable>()->set_model(mc.retrieve("m_ags"));
+		
+		hull->set_main_transform(m);
+		rear_turret->set_priority_transform(midnight::matrixRotation(midnight::Vector3{0, 1, 0}, std::numbers::pi_v<float> / -4));
+		forward_turret->set_priority_transform(midnight::matrixRotation(midnight::Vector3{0, 1, 0}, std::numbers::pi_v<float> / 3));
+		rear_turret->set_main_transform(midnight::matrixTranslation(midnight::Vector3{-0.61, 0.15, 0}));
+		forward_turret->set_main_transform(midnight::matrixTranslation(midnight::Vector3{-1.15, 0.15, 0}));
 
-		glfwSwapBuffers(mw);
-		glfwPollEvents();
+		while (!glfwWindowShouldClose(mw)) {
+			gl::glClear(gl::GL_COLOR_BUFFER_BIT | gl::GL_DEPTH_BUFFER_BIT);
 
-		std::this_thread::sleep_for(std::chrono::milliseconds{100});
+			const float t{static_cast<const float>(glfwGetTime())};
+			midnight::Vector3 dir{midnight::cartesian3({1, std::sin(t), std::sin(t)})};
+			gl::glUniform3fv(gl::glGetUniformLocation(program, "u_light_dir"), 1, dir.dataPtr());
+			// m *= midnight::matrixRotation(midnight::Vector3{0, 1, 0}, 0.1);
+			
+			main_scene.cycle();
+			rear_turret->transform_priority(midnight::matrixRotation(midnight::Vector3{0, 1, 0}, 0.1));
+
+			glfwSwapBuffers(mw);
+			glfwPollEvents();
+
+			std::this_thread::sleep_for(std::chrono::milliseconds{100});
+		}
 	}
 
 	glfwDestroyWindow(mw);
